5635aef2ec57bf9db46e0deb686292b33b09e48d
Zhenia <Zhenia>
2021-06-27 16:01:24 +0200

    images paths correct

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -3,52 +3,52 @@ import "../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 
 const images = [
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/2.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/2.jpg",
+    original: "/gallery/1.jpg",
+    thumbnail: "/gallery/1.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/3.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/3.jpg",
+    original: "/gallery/2.jpg",
+    thumbnail: "/gallery/2.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/4.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/4.jpg",
+    original: "/gallery/3.jpg",
+    thumbnail: "/gallery/3.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/1.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/1.jpg",
+    original: "/gallery/4.jpg",
+    thumbnail: "/gallery/4.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/5.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/5.jpg",
+    original: "/gallery/5.jpg",
+    thumbnail: "/gallery/5.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/6.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/6.jpg",
+    original: "/gallery/6.jpg",
+    thumbnail: "/gallery/6.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/7.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/7.jpg",
+    original: "/gallery/7.jpg",
+    thumbnail: "/gallery/7.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/8.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/8.jpg",
+    original: "/gallery/8.jpg",
+    thumbnail: "/gallery/8.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/9.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/9.jpg",
+    original: "/gallery/9.jpg",
+    thumbnail: "/gallery/9.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/10.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/10.jpg",
+    original: "/gallery/10.jpg",
+    thumbnail: "/gallery/10.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/11.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/11.jpg",
+    original: "/gallery/11.jpg",
+    thumbnail: "/gallery/11.jpg",
   },
   {
-    original: "http://noclegiwrzesnia.com.pl/images/galeria/12.jpg",
-    thumbnail: "http://noclegiwrzesnia.com.pl/images/galeria/12.jpg",
+    original: "/gallery/12.jpg",
+    thumbnail: "/gallery/12.jpg",
   },
 ];
 
