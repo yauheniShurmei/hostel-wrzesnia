da0c366f6bbf8061d2b4fd8a0639495afa596f87
Zhenia <Zhenia>
2021-06-28 14:58:13 +0200

    add nav bar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,14 +1,25 @@
 import classes from "./Navbar.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
+
+  let menuClasses = [classes.navLink, classes.CloseMenu];
+  isOpenBurgerMenu ? (menuClasses[1] = classes.OpenMenu) : null;
+
+  const onOpenMenuHandler = () => {
+    setIsOpenBurgerMenu(!isOpenBurgerMenu);
+    console.log(menuClasses);
+  };
+
   return (
     <header className={classes.Navbar}>
       <main>
-        <div>
+        <div className={classes.Logo}>
           <img src="logo_arcadia.png" />
         </div>
-        <nav>
+        <nav className={menuClasses.join(" ")} onClick={onOpenMenuHandler}>
           <Link href="/strona-glowna">STRONA GŁÓWNA</Link>
           <Link href="#">OFERTA</Link>
           <Link href="/galeria">GALERIA</Link>
@@ -18,6 +29,13 @@ const Navbar = () => {
             {/* <Link href="#">RUS</Link> */}
           </span>
         </nav>
+        <section className={classes.BurgerMenuSection}>
+          <div onClick={onOpenMenuHandler} className={classes.BurgerMenuDiv}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </section>
       </main>
     </header>
   );
